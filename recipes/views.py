6e61982160521d3ddf45f9b4a097c76e6da4c0de
Wilian Ziviani Ovani <wilianzivi007@gmail.com>
2025-10-18 22:41:13 -0300

    exemplo de template

---
+++ recipes/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home (request):
+    ...
+    return render(request, 'recipes/home.html', context={
+        'name': 'Wilian Ziviani'
+    })
+
+
+def contato (request):
+    ...
+    return HttpResponse('contato')
+
+
+def sobre (request):
+    ...
+    return HttpResponse('sobre')
